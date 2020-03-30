d32f80670050dca9123d752c63494b6076844957
Mykola <alexeevn@gmail.com>
2020-03-30 22:32:48 +0300

    added API Gateway

--- pc/src/main/java/org/nipu/pc/GreetingService.java
+++ pc/src/main/java/org/nipu/pc/GreetingService.java
@@ -1,12 +1,16 @@
 package org.nipu.pc;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingService {
+    @Value("${server.port}")
+    private String serverPort;  // set from command line to allow multiple instances run
+
     @RequestMapping("/greeting")
     public String greeting() {
-        return "Hello from product catalog2";
+        return "Hello from product catalog:" + serverPort;
     }
 }
