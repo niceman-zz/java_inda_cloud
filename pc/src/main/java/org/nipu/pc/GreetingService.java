ea63bf0acd6fa6d7c6d0452abe84fa36ac544da3
Mykola <alexeevn@gmail.com>
2020-04-05 16:55:09 +0300

    added logger (ready for elk)

--- pc/src/main/java/org/nipu/pc/GreetingService.java
+++ pc/src/main/java/org/nipu/pc/GreetingService.java
@@ -1,16 +1,22 @@
 package org.nipu.pc;
 
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingService {
+    private static final Logger log = (Logger) LoggerFactory.getLogger(GreetingService.class);
+
     @Value("${server.port}")
     private String serverPort;  // set from command line to allow multiple instances run
 
     @RequestMapping("/greeting")
     public String greeting() {
-        return "Hello from product catalog:" + serverPort;
+        String msg = "Hello from product catalog:" + serverPort;
+        log.info(msg);
+        return msg;
     }
 }
--- po/src/main/java/org/nipu/po/GreetingController.java
+++ po/src/main/java/org/nipu/po/GreetingController.java
@@ -1,17 +1,22 @@
 package org.nipu.po;
 
+import ch.qos.logback.classic.Logger;
 import org.nipu.po.order.clients.GreetingService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+    private static final Logger log = (Logger) LoggerFactory.getLogger(GreetingController.class);
+
     @Autowired
     private GreetingService greetingService;
 
     @RequestMapping(path="/greeting")
     public String greeting() {
+        log.info("Greeting service has been called");
         return greetingService.greeting();
     }
 }
